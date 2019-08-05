cd94dc9bbdc8d2325d9422f8a8df9540ab11c19d
Roman Rabcheniuk <roman_rabcheniuk@epam.com>
2019-08-05 18:05:30 +0300

    react bundle

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+
+// const App = () => {
+//   return <div>App</div>;
+// };
+
+render(<App />, document.getElementById("root"));
